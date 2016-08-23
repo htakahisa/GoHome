cd790371c6f2429a17b3f84a063a93b31c390465
takahisa007 <ronethousand@gmail.com>
2016-08-23 21:10:38 +0900

    Initial commit

---
+++ app/src/main/java/knowledge/prime/gohome/MainActivity.java
@@ -0,0 +1,29 @@
+package knowledge.prime.gohome;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SENDTO);
+
+        intent.setType("text/plain");
+        intent.setData(Uri.parse("mailto:chigogahuchi@ezweb.ne.jp"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Re:");
+        intent.putExtra(Intent.EXTRA_TEXT, "とうちんそろそろかえるよ");
+
+        startActivity(intent);
+
+
+        this.finish();
+
+    }
+}
